3c4fffff9786ccd4d9bb90af0c589a9b6609807e
Dimitris <dimitris.dimtriadis@gmail.com>
2022-02-05 16:05:09 +0200

    v0.18: New beginning

---
+++ Hackerank/Java/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Java_Stdin_and_Stdout_I {
+
+    public static void main(String args[]) throws FileNotFoundException {
+        
+        File file=new File("E:\\SourceTree\\Algorithm-Samples\\Hackerank\\Java\\Input.txt");
+        Scanner sc=new Scanner(file);        
+                
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+
+        sc.close();        
+    }
+}
